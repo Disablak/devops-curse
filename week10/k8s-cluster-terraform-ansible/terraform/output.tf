2cf581f8f66517294c01a4d8db3d95f500d62d78
Max Verchynskyi <disablaksteam@gmail.com>
2025-07-10 13:27:57 +0300

    configuring machines

--- week10/k8s-cluster-terraform-ansible/terraform/output.tf
+++ week10/k8s-cluster-terraform-ansible/terraform/output.tf
@@ -6,11 +6,28 @@ output "instance_ips" {
 }
 
 resource "local_file" "machines" {
-    
   content = templatefile("machines.txt.tftpl", {
     server-ip = aws_instance.debian_nodes["server"].public_ip,
     node-0-ip = aws_instance.debian_nodes["node0"].public_ip,
     node-1-ip = aws_instance.debian_nodes["node1"].public_ip
   })
-  filename = "../machines.txt"
+  filename = "../ansible/machines.txt"
+}
+
+resource "local_file" "inventory" {
+  content = templatefile("inventory.tftpl", {
+    server-ip = aws_instance.debian_nodes["server"].public_ip,
+    node-0-ip = aws_instance.debian_nodes["node0"].public_ip,
+    node-1-ip = aws_instance.debian_nodes["node1"].public_ip
+  })
+  filename = "../ansible/inventory"
+}
+
+resource "local_file" "hosts" {
+  content = templatefile("hosts.tftpl", {
+    server-ip = aws_instance.debian_nodes["server"].public_ip,
+    node-0-ip = aws_instance.debian_nodes["node0"].public_ip,
+    node-1-ip = aws_instance.debian_nodes["node1"].public_ip
+  })
+  filename = "../ansible/hosts"
 }
